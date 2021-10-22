95f5265857f468154d4ca259c868b214d9a8d3f8
shuzo-5341 <shuu.kamekiti0524@icloud.com>
2021-10-22 18:19:29 +0900

    完成

---
+++ main.js
@@ -0,0 +1,44 @@
+//var img = document.getElementById("image_file");
+
+//img.src = "imgages/kawahagi.jpg";
+
+
+
+    function kawahagi1(){
+    img = document.getElementById("image_file1");
+    img.src = "images/kawahagi.jpg";
+    img.width=300;
+    img.height=300;
+    }
+
+
+
+    function akou1(){
+    img = document.getElementById("image_file1");
+    img.src = "images/akou.jpg";
+    img.width=300;
+    img.height=300;
+    }
+/*const AddButton = document.getElementById("add-button")
+const img = documnt.getElementById("image-file")
+
+AddButton.onclick = function(){
+    console.log("%c ",'background: (images/kawahagi.jpg)')
+}*/
+
+
+    function fd3s1(){
+    img = document.getElementById("image_file2");
+    img.src = "images/fd3s.jpg";
+    img.width=300;
+    img.height=300;
+    }
+
+   
+
+    function roadster1(){
+    img = document.getElementById("image_file2");
+    img.src = "images/roadster.jpg";
+    img.width=300;
+    img.height=300;
+    }
